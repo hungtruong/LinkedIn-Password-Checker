e19fd8453f14155ac4f08be9c92a899da65fd6c2
Jon Ericson <jon@jlericson.com>
2021-09-06 21:06:12 -0700

    Update to work with Python3

--- linkedin.py
+++ linkedin.py
@@ -4,17 +4,17 @@ import re
 
 p = getpass.getpass("Enter your password (it won't be displayed): ")
 
-hex = hashlib.sha1(p).hexdigest()
+hex = hashlib.sha1(p.encode('utf-8')).hexdigest()
 hexshort = hex[6:]
 
-passwords = open('combo_not.txt', 'r')
+passwords = open('combo_not.txt', 'r', encoding='utf-8')
 
 for password in passwords:
   if re.search(hex,password):
-    print "The password you typed in was found in the leaked database, but it hasn't yet been cracked in this version of the list. You should probably change your LinkedIn password."
+    print("The password you typed in was found in the leaked database, but it hasn't yet been cracked in this version of the list. You should probably change your LinkedIn password.")
     exit()
   if re.search(hexshort,password):
-    print "The password you typed in was found in the leaked database, and it was already cracked. You should probably change your LinkedIn password."
+    print("The password you typed in was found in the leaked database, and it was already cracked. You should probably change your LinkedIn password.")
     exit()
 
-print "The password you typed was NOT found in the leaked database. You should change your LinkedIn password anyway."
+print("The password you typed was NOT found in the leaked database. Wouldn't hurt to change your LinkedIn password anyway.")
